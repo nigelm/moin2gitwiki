932b3143e752399f8fae83d65450da457ab6afab
Nigel Metheringham <nigelm@cpan.org>
2020-12-31 10:56:03 +0000

    Modified fetch_cache to use Path classes

--- moin2gitwiki/fetch_cache.py
+++ moin2gitwiki/fetch_cache.py
@@ -1,6 +1,6 @@
 import json
-import os
 import uuid
+from pathlib import Path
 
 import attr
 import requests
@@ -8,25 +8,27 @@ import requests
 
 @attr.s(kw_only=True, slots=True)
 class FetchCache:
-    cache_directory: str = attr.ib()
-    index_path: str = attr.ib()
+    cache_directory: Path = attr.ib()
+    index_path: Path = attr.ib()
     cache_map: dict = attr.ib(default={})
     ctx = attr.ib(repr=False)
 
     @classmethod
-    def initialise_cache(cls, cache_directory: str, ctx):
+    def initialise_cache(cls, cache_directory: Path, ctx):
         # ensure directory exists
-        os.makedirs(cache_directory, mode=0o777, exist_ok=True)
+        cache_directory.mkdir(mode=0o777, parents=True, exist_ok=True)
+        # ensure we have it as an absolute path
+        cache_directory = cache_directory.resolve(strict=True)
         #
         # load the index should it exist
-        index_path = os.path.join(cache_directory, "index.json")
+        index_path = cache_directory.joinpath("index.json")
         cache_map = {}
         try:
-            with open(index_path) as f:
-                cache_map = json.load(f)
+            cache_map = json.loads(index_path.read_text())
         except (OSError, ValueError):
             # if anything goes wrong then we just ignore it and
             # write out a blank cache file
+            cache_map = {}
             cls.write_index(index_path=index_path, cache_map=cache_map)
         #
         # build and return the object
@@ -39,39 +41,36 @@ class FetchCache:
         )
 
     @classmethod
-    def write_index(cls, index_path: str, cache_map: dict):
-        with open(index_path, "w") as f:
-            json.dump(cache_map, f, indent=2)
+    def write_index(cls, index_path: Path, cache_map: dict):
+        index_path.write_text(json.dumps(cache_map, indent=2))
 
     def fetch(self, url: str):
         #
         # is this in the cache already
         if url in self.cache_map:
             item_name = self.cache_map[url]
-            item_path = os.path.join(self.cache_directory, item_name)
+            item_path = self.cache_directory.joinpath(item_name)
             try:
-                with open(item_path) as f:
-                    content = f.readlines()
+                lines = item_path.read_text().splitlines(keepends=True)
                 self.ctx.logger.debug(f"Retrieved {url} from cache")
-                return content
+                return lines
             except OSError:
                 pass  # just move on to refetch
         #
         # if you get here then the url is either not in the cache or we
         # failed to retrieve it off disk - in either case we just fetch it
         item_name = uuid.uuid4().hex
-        item_path = os.path.join(self.cache_directory, item_name)
+        item_path = item_path = self.cache_directory.joinpath(item_name)
         self.ctx.logger.debug(f"Fetching {url}")
         try:
             response = requests.get(url)
-            content = response.text.splitlines(keepends=True)
+            content = response.text
         except OSError:
             self.ctx.logger.warning(f"No response to {url}")
-            content = ["\n"]
+            content = ""
         #
         # write to cache
-        with open(item_path, "w") as f:
-            f.writelines(content)
+        item_path.write_text(content)
         self.ctx.logger.debug(f"Wrote {url} to {item_name}")
         #
         # update cache index
@@ -79,7 +78,7 @@ class FetchCache:
         self.write_index(index_path=self.index_path, cache_map=self.cache_map)
         #
         # return response as a set of lines
-        return content
+        return content.splitlines(keepends=True)
 
 
 # end
